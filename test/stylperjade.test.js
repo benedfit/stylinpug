4e5462c86c1d51e1d6c6f1b1bf9968d6cf992efb
Ben Edwards <ben.edwards@clock.co.uk>
2015-02-24 14:47:07 +0000

    > Update tests to include error checking and results of using whitelist

--- test/stylperjade.test.js
+++ test/stylperjade.test.js
@@ -1,19 +1,126 @@
-var glob = require('glob')
+var assert = require('assert')
+  , glob = require('glob')
   , stylperjade = require('../lib/stylperjade')
-  , assert = require('assert')
-  , fixturesPath = __dirname + '/fixtures/'
 
 describe('stylperjade', function () {
 
-  it('should not error on simple test case', function (done) {
+  var fixturesPath = __dirname + '/fixtures/'
+
+  it('should error if no CSS files specified', function (done) {
+    var cssFiles = []
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+
+    assert.throws(function () {
+      stylperjade(cssFiles, jadeFiles, function (err, output) {
+        assert.equal(output, null)
+      })
+    }
+    , /Stylperjade: no CSS files found/)
+    done()
+  })
+
+  it('should error if no Jade files specified', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ ]
+
+    assert.throws(function () {
+      stylperjade(cssFiles, jadeFiles, function (err, output) {
+        assert.equal(output, null)
+      })
+    }
+    , /Stylperjade: no Jade files found/)
+    done()
+  })
+
+  it('should error if no callback specified', function (done) {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
-      , options = {}
+
+    assert.throws(function () {
+      stylperjade(cssFiles, jadeFiles)
+    }
+    , /Stylperjade: expected a callback/)
+    done()
+  })
+
+  it('should return error if no CSS files found', function (done) {
+    var cssFiles = [ 'nonexistent' ]
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+
+      stylperjade(cssFiles, jadeFiles, function (err, output) {
+        assert(err)
+        assert.equal(err, 'Stylperjade: CSS file \'nonexistent\' not found')
+        assert.equal(output, null)
+        done()
+      })
+  })
+
+  it('should return error if no Jade files found', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ 'nonexistent' ]
+
+      stylperjade(cssFiles, jadeFiles, function (err, output) {
+        assert(err)
+        assert.equal(err, 'Stylperjade: Jade file \'nonexistent\' not found')
+        assert.equal(output, null)
+        done()
+      })
+  })
+
+  it('should return unused CSS and Jade classes', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+
+    stylperjade(cssFiles, jadeFiles, function (err, output) {
+      assert(!err)
+      assert.equal(output.total, 9)
+      assert.equal(output.cssCount, 4)
+      assert.equal(output.cssClasses.indexOf('delta') !== -1, true)
+      assert.equal(output.cssClasses.indexOf('bravo') === -1, true)
+      assert.equal(output.jadeCount, 5)
+      assert.equal(output.jadeClasses.indexOf('epsilon') !== -1, true)
+      assert.equal(output.jadeClasses.indexOf('bravo') === -1, true)
+      done()
+    })
+  })
+
+  it('should filter out CSS classes using options.cssWhitelist', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+      , options = { cssWhitelist: [ 'delta*', 'zeta' ] }
 
     stylperjade(cssFiles, jadeFiles, options, function (err, output) {
-      if (err) done(err)
-      console.log(output)
       assert(!err)
+      assert.equal(output.total, 6)
+      assert.equal(output.cssCount, 1)
+      assert.equal(output.cssClasses.indexOf('delta') === -1, true)
+      assert.equal(output.cssClasses.indexOf('delta-another') === -1, true)
+      assert.equal(output.cssClasses.indexOf('zeta') === -1, true)
+      assert.equal(output.cssClasses.indexOf('nested') !== -1, true)
+      assert.equal(output.jadeCount, 5)
+      assert.equal(output.jadeClasses.indexOf('epsilon') !== -1, true)
+      assert.equal(output.jadeClasses.indexOf('bravo') === -1, true)
+      done()
+    })
+  })
+
+  it('should filter out Jade classes using options.jadeWhitelist', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+      , options = { jadeWhitelist: [ 'js-*' ] }
+
+    stylperjade(cssFiles, jadeFiles, options, function (err, output) {
+      assert(!err)
+      assert.equal(output.total, 6)
+      assert.equal(output.cssCount, 4)
+      assert.equal(output.cssClasses.indexOf('delta') !== -1, true)
+      assert.equal(output.cssClasses.indexOf('bravo') === -1, true)
+      assert.equal(output.jadeCount, 2)
+      assert.equal(output.jadeClasses.indexOf('epsilon') !== -1, true)
+      assert.equal(output.jadeClasses.indexOf('bravo') === -1, true)
+      assert.equal(output.jadeClasses.indexOf('js-other-thing') === -1, true)
+      assert.equal(output.jadeClasses.indexOf('js-test') === -1, true)
+      assert.equal(output.jadeClasses.indexOf('js-thing-1') === -1, true)
       done()
     })
   })
