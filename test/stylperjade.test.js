11f11babe21ba1f947df581e6b1a6a8f229d26a6
Ben Edwards <ben.edwards@clock.co.uk>
2015-02-25 19:19:41 +0000

    Additional report format tests

--- test/stylperjade.test.js
+++ test/stylperjade.test.js
@@ -1,4 +1,5 @@
 var assert = require('assert')
+  , chalk = require('chalk')
   , fs = require('fs')
   , stylperjade = require('../lib/stylperjade')
 
@@ -70,7 +71,7 @@ describe('stylperjade', function () {
   it('should return unused CSS and Jade classes', function (done) {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
-      , expectedReport = fs.readFileSync(fixturesPath + 'test.txt')
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected.txt', 'utf-8')
 
     stylperjade(cssFiles, jadeFiles, function (err, results) {
       assert(!err)
@@ -78,6 +79,8 @@ describe('stylperjade', function () {
       assert.equal(results.unusedCssCount, 5)
       assert.equal(results.unusedJadeCount, 10)
       assert.equal(results.blacklistedTotal, 0)
+      assert.equal(results.blacklistedCssCount, 0)
+      assert.equal(results.blacklistedJadeCount, 0)
       assert.equal(results.unusedCssClasses.indexOf('delta--modifier') !== -1, true)
       assert.equal(results.unusedCssClasses.indexOf('kappa') !== -1, true)
       assert.equal(results.unusedCssClasses.indexOf('pi') !== -1, true)
@@ -86,15 +89,38 @@ describe('stylperjade', function () {
       assert.equal(results.unusedJadeClasses.indexOf('epsilon') !== -1, true)
       assert.equal(results.unusedJadeClasses.indexOf('js-alpha') !== -1, true)
       assert.equal(results.unusedJadeClasses.indexOf('beta') === -1, true)
-      assert.equal(results.report, expectedReport)
+      assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
       done()
     })
+  })
+
+  it('should report if no unused CSS and Jade classes', function (done) {
+    var cssFiles = [ fixturesPath + 'test.css' ]
+      , jadeFiles = [ fixturesPath + 'test.jade' ]
+      , options =
+        { cssWhitelist: [ '*' ]
+        , jadeWhitelist: [ '*' ]
+        }
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected-none.txt', 'utf-8')
+
+      stylperjade(cssFiles, jadeFiles, options, function (err, results) {
+        assert(!err)
+        assert.equal(results.unusedTotal, 0)
+        assert.equal(results.unusedCssCount, 0)
+        assert.equal(results.unusedJadeCount, 0)
+        assert.equal(results.blacklistedTotal, 0)
+        assert.equal(results.blacklistedCssCount, 0)
+        assert.equal(results.blacklistedJadeCount, 0)
+        assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
+        done()
+      })
   })
 
   it('should filter out CSS classes using options.cssWhitelist', function (done) {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
       , options = { cssWhitelist: [ 'delta*', 'kappa' ] }
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected-csswhitelist.txt', 'utf-8')
 
     stylperjade(cssFiles, jadeFiles, options, function (err, results) {
       assert(!err)
@@ -103,6 +129,7 @@ describe('stylperjade', function () {
       assert.equal(results.unusedJadeCount, 10)
       assert.equal(results.unusedCssClasses.indexOf('delta--modifier') === -1, true)
       assert.equal(results.unusedCssClasses.indexOf('kappa') === -1, true)
+      assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
       done()
     })
   })
@@ -111,6 +138,7 @@ describe('stylperjade', function () {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
       , options = { jadeWhitelist: [ 'js-*' ] }
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected-jadewhitelist.txt', 'utf-8')
 
     stylperjade(cssFiles, jadeFiles, options, function (err, results) {
       assert(!err)
@@ -120,6 +148,7 @@ describe('stylperjade', function () {
       assert.equal(results.unusedJadeClasses.indexOf('js-alpha') === -1, true)
       assert.equal(results.unusedJadeClasses.indexOf('js-beta-delta') === -1, true)
       assert.equal(results.unusedJadeClasses.indexOf('js-mu') === -1, true)
+      assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
       done()
     })
   })
@@ -128,14 +157,17 @@ describe('stylperjade', function () {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
       , options = { cssBlacklist: [ 'delta*', 'kappa' ] }
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected-cssblacklist.txt', 'utf-8')
 
     stylperjade(cssFiles, jadeFiles, options, function (err, results) {
       assert(!err)
       assert.equal(results.blacklistedTotal, 3)
       assert.equal(results.blacklistedCssCount, 3)
+      assert.equal(results.blacklistedJadeCount, 0)
       assert.equal(results.blacklistedCssClasses.indexOf('delta') !== -1, true)
       assert.equal(results.blacklistedCssClasses.indexOf('delta--modifier') !== -1, true)
       assert.equal(results.blacklistedCssClasses.indexOf('kappa') !== -1, true)
+      assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
       done()
     })
   })
@@ -144,14 +176,17 @@ describe('stylperjade', function () {
     var cssFiles = [ fixturesPath + 'test.css' ]
       , jadeFiles = [ fixturesPath + 'test.jade' ]
       , options = { jadeBlacklist: [ 'js-*' ] }
+      , expectedReport = fs.readFileSync(fixturesPath + 'expected-jadeblacklist.txt', 'utf-8')
 
     stylperjade(cssFiles, jadeFiles, options, function (err, results) {
       assert(!err)
       assert.equal(results.blacklistedTotal, 3)
+      assert.equal(results.blacklistedCssCount, 0)
       assert.equal(results.blacklistedJadeCount, 3)
       assert.equal(results.blacklistedJadeClasses.indexOf('js-alpha') !== -1, true)
       assert.equal(results.blacklistedJadeClasses.indexOf('js-beta-delta') !== -1, true)
       assert.equal(results.blacklistedJadeClasses.indexOf('js-mu') !== -1, true)
+      assert.equal(chalk.stripColor(results.report + '\n'), expectedReport)
       done()
     })
   })
